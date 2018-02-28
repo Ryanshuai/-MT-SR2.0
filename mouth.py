1b3b628c62252032cedd8cd108a8d69502f77f9b
Ryan <1018718155@qq.com>
2018-02-28 17:45:16 +0800

    去掉mt内部文件。

--- mouth.py
+++ mouth.py
@@ -1,12 +1,15 @@
-import MTLabFace
+from FDFA import MTLabFace
 from PIL import Image, ImageDraw
-
+from torchvision.transforms import ToPILImage, ToTensor
 
 
 def get_mouth_mask(im):
+    if not Image.isImageType(im):
+        im = ToPILImage()(im)
 
-    mask = Image.new ('RGB', im.size, (255, 255, 255))
+    mask = Image.new('RGB', im.size, (0,0,0))
     mask_draw = ImageDraw.Draw(mask)
+
 
     config=MTLabFace.MTFaceConfig()
     config.MeituFA=True
@@ -17,10 +20,10 @@ def get_mouth_mask(im):
     Points=face["Points"]   # fa will return 118 points
 
 
-    left, up, right, low = im.width, im.height, 0, 0
     mouth_points = Points[86:98]
+    mp = mouth_points
 
-    # get the relative_mouth_points
+    left, up, right, low = im.width, im.height, 0, 0
     for i in range(len(mouth_points)):
         pos = mouth_points[i]
         left = min(pos[0], left)
@@ -28,22 +31,27 @@ def get_mouth_mask(im):
         right = max(pos[0], right)
         low = max(pos[1], low)
 
-    mp = mouth_points
 
 
     # fill with white up and down the mouth
     for i in range(6):
         x0, y0, x1, y1 = mp[i][0], mp[i][1], mp[i+1][0], mp[i+1][1]
-        mask_draw.polygon([x0, y0, x1, y1, x1, 0, x0, 0], fill="black")
+        mask_draw.polygon([x0, y0, x1, y1, x1, 0, x0, 0], fill="white")
     for i in range(6, 11):
         x0, y0, x1, y1 = mp[i][0], mp[i][1], mp[i+1][0], mp[i+1][1]
-        mask_draw.polygon([x0, y0, x1, y1, x1, im.height, x0, im.height], fill="black")
+        mask_draw.polygon([x0, y0, x1, y1, x1, im.height, x0, im.height], fill="white")
     x0, y0, x1, y1 = mp[11][0], mp[11][1], mp[0][0], mp[0][1]
-    mask_draw.polygon([x0, y0, x1, y1, x1, im.height, x0, im.height], fill="black")
+    mask_draw.polygon([x0, y0, x1, y1, x1, im.height, x0, im.height], fill="white")
 
     # fill with white left and right
-    mask_draw.rectangle([0, 0, left, im.height], fill="black")
-    mask_draw.rectangle([right, 0, im.width, im.height], fill="black")
+    mask_draw.rectangle([0, 0, left, im.height], fill="white")
+    mask_draw.rectangle([right, 0, im.width, im.height], fill="white")
 
+    mask = ToTensor()(mask)
     return mask
 
+if __name__ == '__main__':
+    im = Image.open('test.jpg')
+    mask = get_mouth_mask(im)
+    mask = ToPILImage()(mask)
+    mask.show()
